35e36cde18c77bf8ba29598ada7341aed543cb32
Cristian Brinza <cristian.brinza@isa.utm.md>
2024-07-03 10:12:11 +0300

    Update Button.tsx

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -5,7 +5,7 @@ const Button = ({ onClick, children, className = "", style = {}, disabled = fals
         <div
             className={`smm_main_btn ${className}`}
             onClick={!disabled ? onClick : null}
-            style={{ ...style, opacity: disabled ? 0.5 : 1 }}
+            style={{ ...style, opacity: disabled ? 0.5 : 1, minHeight:'36px' }}
         >
             {children}
         </div>
