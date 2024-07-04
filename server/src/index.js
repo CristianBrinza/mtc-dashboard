2c2bf56cdf0b47a02983f252cf3dd1540bfe7104
Cristian Brinza <cristian.brinza@isa.utm.md>
2024-07-05 00:48:59 +0300

    Add new backend

---
+++ server/src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const swaggerSetup = require('./swagger');
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Routes
+app.use('/api/auth', require('./routes/auth'));
+
+// Swagger
+swaggerSetup(app);
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
