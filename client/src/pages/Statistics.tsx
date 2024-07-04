b3fe6d003d198a8c7f487816ee9c0d6d2c2f7d59
Cristian Brinza <cristian.brinza@isa.utm.md>
2024-07-04 21:44:44 +0300

    Create Statistics.tsx

---
+++ client/src/pages/Statistics.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "../styles/utilities.css"
+
+export default function Statistics() {
+    const navigate = useNavigate();
+
+    return (
+        <>
+            <div className="utilities_main">
+                <div id="utilities_main_title">Moldtelecom | Marketing Statistics</div>
+
+
+           </div>
+       </>
+    );
+}
