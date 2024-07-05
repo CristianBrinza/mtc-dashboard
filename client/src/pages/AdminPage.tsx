e16fd7b2d9da92439512e3b102e909d6160a028d
Cristian Brinza <cristian.brinza@isa.utm.md>
2024-07-05 11:24:43 +0300

    Create AdminPage.tsx

---
+++ client/src/pages/AdminPage.tsx
@@ -0,0 +1,13 @@
+import Navbar from '../components/Navbar';
+import React from "react";
+
+
+export default function AdminPage() {
+    return (
+        <div>
+            <Navbar />
+Admin
+
+        </div>
+    );
+}
