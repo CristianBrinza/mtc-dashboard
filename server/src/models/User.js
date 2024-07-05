5a01d0546aa11e37f577a81755b1ca2d3018bc52
Cristian Brinza <cristian.brinza@isa.utm.md>
2024-07-05 11:24:24 +0300

    Update User.js

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcryptjs');
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, enum: ['ADMIN', 'WRITER', 'VISITOR'], default: 'VISITOR' },
+    role: { type: String, enum: ['ADMIN', 'SMM', 'DIRECTOR', 'SEF', 'DEFAULT', 'VIEWER'], default: 'VIEWER' },
 });
 
 UserSchema.pre('save', async function (next) {
