ac36dbf7e50f02e7a5d0caa30a81418ffb7fa00a
Cristian Brinza <cristian.brinza@isa.utm.md>
2024-07-05 22:12:21 +0300

    Create Website.tsx

---
+++ client/src/pages/Website.tsx
@@ -0,0 +1,14 @@
+// src/pages/AdminPage.tsx
+import React from 'react';
+import Navbar from "../components/Navbar.tsx";
+
+const Website: React.FC = () => {
+    return (
+        <>
+            <Navbar/>
+
+        </>
+    );
+};
+
+export default Website;
