845f3b74d10669ceedbc2c34cbbbdfcc34115a89
Cristian Brinza <cristian.brinza@isa.utm.md>
2024-07-08 09:48:10 +0300

    Update Home.tsx

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,13 +1,72 @@
 import Navbar from '../components/Navbar';
 import React from "react";
-
+import Icon from "../components/Icon.tsx";
+import "../styles/home.css"
 
 export default function Home() {
-    return (
-        <div>
-            <Navbar />
-home
+    const redirectToExternal = (url) => {
+        window.location.href = url;
+    };
 
-        </div>
+    return (
+        <>
+            <Navbar />
+
+            <div className={"home_main"}>
+                <div id="home_main_title">Moldtelecom Dashboard</div>
+                <div className={"home_empty_space"}>
+                 <span>Usefull link:</span>
+                </div>
+                <div className="home_main_select">
+                    <div className={"home_main_select_block"}
+                         onClick={() => redirectToExternal('https://moldtelecomjsc.sharepoint.com/')}>
+                        <Icon type="sharepoint"/>
+                        <div className={"home_main_select_block_text"}>SharePoint</div>
+                    </div>
+                </div>
+
+                <div className={"home_empty_space"}>
+                   <br/> <span>Social platforms:</span>
+                </div>
+                <div className="home_main_select">
+                    <div className={"home_main_select_block"}
+                         onClick={() => redirectToExternal('https://www.instagram.com/moldtelecom.md/')}>
+                        <Icon type="instagram"/>
+                        <div className={"home_main_select_block_text"}>Instagram</div>
+                    </div>
+                    <div className={"home_main_select_block"}
+                         onClick={() => redirectToExternal('https://www.facebook.com/Moldtelecom/')}>
+                        <Icon type="facebook"/>
+                        <div className={"home_main_select_block_text"}>Facebook</div>
+                    </div>
+                    <div className={"home_main_select_block"}
+                         onClick={() => redirectToExternal('https://www.tiktok.com/@moldtelecom.md')}>
+                        <Icon type="tiktok"/>
+                        <div className={"home_main_select_block_text"}>Tiktok</div>
+                    </div>
+                    <div className={"home_main_select_block"}
+                         onClick={() => redirectToExternal('https://www.linkedin.com/company/moldtelecom')}>
+                        <Icon type="linkedin"/>
+                        <div className={"home_main_select_block_text"}>Linkedin</div>
+                    </div>
+                    <div className={"home_main_select_block"}
+                         onClick={() => redirectToExternal('https://ok.ru/moldtelecom')}>
+                        <Icon type="odnoklassniki"/>
+                        <div className={"home_main_select_block_text"}>Odnoklassniki</div>
+                    </div>
+                    <div className={"home_main_select_block"}
+                         onClick={() => redirectToExternal('https://www.youtube.com/channel/UC7ETXBNXYyQP_FnzeXKlwEQ')}>
+                        <Icon type="youtube"/>
+                        <div className={"home_main_select_block_text"}>Youtube</div>
+                    </div>
+                    <div className={"home_main_select_block"}
+                         onClick={() => redirectToExternal('https://t.me/MoldtelecomMd')}>
+                        <Icon type="telegram"/>
+                        <div className={"home_main_select_block_text"}>Telegram</div>
+                    </div>
+                </div>
+            </div>
+
+        </>
     );
 }
