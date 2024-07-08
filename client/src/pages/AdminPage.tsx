511b646a83c44e07a9a9c330c32ef620cea27b6f
Cristian Brinza <cristian.brinza@isa.utm.md>
2024-07-08 09:48:16 +0300

    Update AdminPage.tsx

--- client/src/pages/AdminPage.tsx
+++ client/src/pages/AdminPage.tsx
@@ -1,12 +1,26 @@
 // src/pages/AdminPage.tsx
 import React from 'react';
 import Navbar from "../components/Navbar.tsx";
+import "../styles/admin.css"
+import Icon from "../components/Icon.tsx";
+import {useNavigate} from "react-router-dom";
 
 const AdminPage: React.FC = () => {
+    const navigate = useNavigate();
+
     return (
         <>
             <Navbar/>
-            <h1>Access Denied</h1>
+           <div className={"admin_main"}>
+               <div id="admin_main_title">Admin Page</div>
+
+               <div id="admin_main_select">
+                   <div className={"admin_main_select_block"}  onClick={() => navigate('/register')}>
+                       <Icon type="add"/>
+New user
+                   </div>
+               </div>
+           </div>
 
         </>
     );
