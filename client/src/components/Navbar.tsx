13c61d5e40f7121132cc67a20d48b8c85434b54b
Cristian Brinza <cristian.brinza@isa.utm.md>
2024-07-08 09:47:42 +0300

    Update Navbar.tsx

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+//src/components/Navbar.tsx
 import React, { useEffect, useState, useContext } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
@@ -36,25 +37,40 @@ const Navbar: React.FC = () => {
         setShowUserMenu(!showUserMenu);
     };
 
-    const initial = userInfo?.username ? userInfo.username.charAt(0).toUpperCase() : '';
+    const getInitials = (username) => {
+        if (!username) return '';
+
+        const parts = username.split('.');
+        if (parts.length === 1) {
+            // If there's only one part (e.g., "cristi"), return the first letter
+            return parts[0].charAt(0).toUpperCase();
+        } else {
+            // If there are multiple parts (e.g., "cristian.brinza"), return the first letter of each part
+            return parts[0].charAt(0).toUpperCase() + parts[1].charAt(0).toUpperCase();
+        }
+    };
+
+    const initials = getInitials(userInfo?.username);
     const avatar = userInfo?.avatar ? (
         <img src={userInfo.avatar} alt="Profile" className="user-avatar" />
     ) : (
-        initial
+        initials
     );
 
     return (
         <div id="mtc_dashboard_menu">
-            <a href="home" style={{ height: "100%" }}>
-                <img id="mtc_dashboard_menu_logo" src="../images/general/logo.png" alt="logo" />
+            <a href="home" style={{height: "100%"}}>
+                <img id="mtc_dashboard_menu_logo" src="../images/general/logo.png" alt="logo"/>
             </a>
+            {userInfo?.role === 'ADMIN' && <a href="/admin">Admin</a>}
+
             {menuData.menu.map((item, index) => (
                 <a key={index} href={item[1]}>{item[0]}</a>
             ))}
             <div
                 className="user-initial-circle"
                 onClick={toggleUserMenu}
-                style={{ marginLeft: 'auto' }}
+                style={{marginLeft: 'auto'}}
             >
                 {avatar}
             </div>
